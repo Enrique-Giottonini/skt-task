1320d1e23332d5c4d0ebb15bc28db552c4081204
Enrique-Giottonini <enrique.giottonini@encora.com>
2024-08-14 09:22:55 -0700

    fix: add lombok annotations

--- entities/src/main/java/com/spark/entities/Product.java
+++ entities/src/main/java/com/spark/entities/Product.java
@@ -1,31 +1,13 @@
 package com.spark.entities;
 
-public class Product {
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Product {
     private long id;
     private String name;
-
-    public Product() {
-    }
-
-    public Product(long id, String name) {
-        this.id = id;
-        this.name = name;
-    }
-
-    public long getId() {
-        return id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
 }
