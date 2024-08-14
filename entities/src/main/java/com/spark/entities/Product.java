6d57edbad5366b365fec39b2613a01263c25ee87
Enrique-Giottonini <enrique.giottonini@encora.com>
2024-08-14 09:12:07 -0700

    feat: show a jsp view from controller

--- entities/src/main/java/com/spark/entities/Product.java
+++ entities/src/main/java/com/spark/entities/Product.java
@@ -1,4 +1,31 @@
 package com.spark.entities;
 
 public class Product {
+
+    private long id;
+    private String name;
+
+    public Product() {
+    }
+
+    public Product(long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
