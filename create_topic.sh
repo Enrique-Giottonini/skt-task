49b751deb32b866c2146783459bdbd61f31cd82c
Enrique-Giottonini <enrique.giottonini@encora.com>
2024-08-20 19:28:47 -0700

    fix: forgot to add script?

---
+++ create_topic.sh
@@ -0,0 +1,2 @@
+/bin/kafka-topics.sh --bootstrap-server kafka:9092 --create --replication-factor 1 --partitions 1 --topic product
+/bin/kafka-topics.sh --bootstrap-server kafka:9092 --create --replication-factor 1 --partitions 1 --topic listOfProducts
