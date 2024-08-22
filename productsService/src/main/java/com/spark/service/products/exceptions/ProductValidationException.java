16d4077c3783b5c5f1380a6431ceb360e5db38b2
Enrique-Giottonini <enrique.giottonini@encora.com>
2024-08-22 09:05:38 -0700

    remove unused method

--- productsService/src/main/java/com/spark/service/products/exceptions/ProductValidationException.java
+++ productsService/src/main/java/com/spark/service/products/exceptions/ProductValidationException.java
@@ -1,10 +1,6 @@
 package com.spark.service.products.exceptions;
 
 public class ProductValidationException extends RuntimeException {
-    public ProductValidationException(String message) {
-        super(message);
-    }
-
     public ProductValidationException(String message, Throwable cause) {
         super(message, cause);
     }
