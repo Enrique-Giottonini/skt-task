f93759b88df7a96184a4efcf6882ebe95d7fa127
Enrique-Giottonini <enrique.giottonini@encora.com>
2024-08-21 19:46:31 -0700

    feat: add minimal documentation to addProduct

--- productsService/src/main/java/com/spark/service/products/ProductService.java
+++ productsService/src/main/java/com/spark/service/products/ProductService.java
@@ -1,9 +1,19 @@
 package com.spark.service.products;
 
 import com.spark.entities.domain.ProductDTO;
+import com.spark.service.products.exceptions.ProductValidationException;
+import org.hibernate.exception.DataException;
 
 public interface ProductService {
+    /**
+     * Adds a new product to the repository.
+     *
+     *
+     * @param product the product dto containing the information of the product to add
+     * @throws ProductValidationException if the product data is invalid
+     * @throws DataException if the product could not be saved to the database
+     */
     void addProduct(ProductDTO product);
-    void notifyUpdatedList(); // TODO: documentation?
+    void notifyUpdatedList(); // Todo: documentation
     void resendList();
 }
