30f0b4b513b7095eb1ac96f49a7f463ecc36a66a
Enrique-Giottonini <enrique.giottonini@encora.com>
2024-08-23 10:30:21 -0700

    excluded testing in files

---
+++ product-service/src/test/java/com/spark/service/products/ProductsServiceAppTest.java
@@ -0,0 +1,13 @@
+package com.spark.service.products;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+public class ProductsServiceAppTest {
+
+    @Test
+    public void contextLoads() {
+    }
+}
