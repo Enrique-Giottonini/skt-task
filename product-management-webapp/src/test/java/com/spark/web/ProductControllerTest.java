1e73205c18404f3dc7dd9d3a48c8455b8244282b
Enrique-Giottonini <enrique.giottonini@encora.com>
2024-08-26 15:09:57 -0700

    test: finish web controller layer

--- product-management-webapp/src/test/java/com/spark/web/ProductControllerTest.java
+++ product-management-webapp/src/test/java/com/spark/web/ProductControllerTest.java
@@ -12,9 +12,13 @@ import org.apache.kafka.common.KafkaException;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.math.BigDecimal;
+import java.util.Arrays;
 import java.util.Collections;
+import java.util.List;
 
 import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
@@ -27,6 +31,30 @@ public class ProductControllerTest {
 
     @MockBean
     private ProductServiceImpl productService;
+
+    @Test
+    public void testList() throws Exception {
+        ProductDTO product1 = new ProductDTO(1L, "Product 1", "Description 1", BigDecimal.valueOf(10.00));
+        ProductDTO product2 = new ProductDTO(2L, "Product 2", "Description 2", BigDecimal.valueOf(20.00));
+        List<ProductDTO> mockProductList = Arrays.asList(product1, product2);
+
+        when(productService.findAll()).thenReturn(mockProductList);
+
+        mockMvc.perform(get("/product"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("product-list"))
+                .andExpect(model().attributeExists("listOfProduct"))
+                .andExpect(model().attribute("listOfProduct", mockProductList));
+    }
+
+    @Test
+    public void testNewProduct() throws Exception {
+        mockMvc.perform(get("/product/new"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("product-add"))
+                .andExpect(model().attributeExists("product"))
+                .andExpect(model().attribute("product", new ProductDTO()));
+    }
 
     @Test
     public void testAddProduct_Success() throws Exception {
